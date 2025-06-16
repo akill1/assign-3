16787c77c688d156ca13afbed08e8e9c5bbf233b
akhil <akhilpichukala@gmail.com>
2025-06-16 11:19:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MetricsGrid from './components/metrics';
+
+function App() {
+  return (
+    <div className='App'>
+        <MetricsGrid/>
+    </div>
+  );
+}
+
+export default App;
